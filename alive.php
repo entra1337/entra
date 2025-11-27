a81d88a04b301185bdd49a4bb85ddcc30b81f6bf
entra1337 <manchalic@protonmail.com>
2025-11-27 01:22:01 -0500

    Update alive.php

--- alive.php
+++ alive.php
@@ -1,5 +1,8 @@
 <?php
-// Check if the cookie named 'auth' (or whatever name you want) is set and equals 'toot'
+header('Content-Type: text/plain');
+http_response_code(200);
+
+// Check if the cookie named 'auth' is set and equals 'toot'
 if (isset($_COOKIE['auth']) && $_COOKIE['auth'] === 'toot') {
     echo "im alive";
 } else {
